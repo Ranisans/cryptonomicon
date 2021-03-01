65a5000894df5d16b45533d68aa7dd32b710342e
ranisans <ranisans@yahoo.com>
2021-03-01 12:21:36 +0300

    refact: split mono app to components

---
+++ src/components/AppButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    :disabled="disabled"
+    :class="disabled && 'cursor-not-allowed opacity-50'"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+};
+</script>
